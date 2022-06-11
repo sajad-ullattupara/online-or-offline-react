0678c659f90211967d1c4246be5cffa39dc8da9d
sajad-ullattupara <106068240+sajad-ullattupara@users.noreply.github.com>
2022-06-11 16:39:55 +0530

    App.js Updated
    Added Alternative Program

--- src/App.js
+++ src/App.js
@@ -11,3 +11,21 @@ export default function App() {
   <h1 className="istatus">You are {online ? "online" : "offline"}</h1>
   </div>;
 }
+
+
+// // Alternative Program
+
+// import React, { useEffect } from "react";
+// import { useOnlineStatus, usePrevious } from "react-hookedup";
+
+// export default function App() {
+//   const { online } = useOnlineStatus();
+//   const prevOnline = usePrevious(online);
+//   useEffect(() => {
+//     if (prevOnline === false && online === true) {
+//       alert("syncing data");
+//     }
+//   }, [prevOnline, online]);
+  
+//   return <div>You are {online ? "online" : "offline"}!</div>;
+// }
